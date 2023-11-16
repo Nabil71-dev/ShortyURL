7b7eb7b71f9ee2009b807e455e6d08f942dcaba9
Nabil71-dev <mahmudnabiliiuc@gmail.com>
2023-11-16 17:15:34 +0600

    small changes in routes and config file added

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    rewrites : [
+        {
+            source: '/:catchAll(.*)',
+            destination: '/',
+        },
+    ],
     env: {
         // # Server URL
         SERVER_URL: process.env.SERVER_URL,
 
         // # Firebase credentials
-        TOKEN_SECRET_KEY:process.env.TOKEN_SECRET_KEY,
-        API_KEY:process.env.API_KEY,
-        auth_Domain:process.env.auth_Domain,
-        project_Id:process.env.project_Id,
-        storage_Bucket:process.env.storage_Bucket,
-        messaging_Sender_Id:process.env.messaging_Sender_Id,
-        app_Id:process.env.app_Id,
+        TOKEN_SECRET_KEY: process.env.TOKEN_SECRET_KEY,
+        API_KEY: process.env.API_KEY,
+        auth_Domain: process.env.auth_Domain,
+        project_Id: process.env.project_Id,
+        storage_Bucket: process.env.storage_Bucket,
+        messaging_Sender_Id: process.env.messaging_Sender_Id,
+        app_Id: process.env.app_Id,
     },
 }
 
-module.exports = nextConfig
+module.exports =  nextConfig
