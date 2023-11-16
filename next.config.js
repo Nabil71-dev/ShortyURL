9263268f5a5d28e76327985b1c33125ba2231aa8
Nabil71-dev <mahmudnabiliiuc@gmail.com>
2023-11-16 13:22:16 +0600

    added some change on envs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
+        // # Server URL
+        SERVER_URL: 'https://shortyurl-server.vercel.app/',
 
+        // # Firebase credentials
+        TOKEN_SECRET_KEY: 'shortyUrlSecretToken',
+        API_KEY: 'AIzaSyDIMeEXLkCgWAOPiwlXZ2r44k9ueXHbXTs',
+        auth_Domain: 'shorty-url-imp.firebaseapp.com',
+        project_Id: 'shorty-url-imp',
+        storage_Bucket: 'shorty-url-imp.appspot.com',
+        messaging_Sender_Id: '459818517169',
+        app_Id: '1:459818517169:web:4e7372a73cd0ef2de05890',
     },
 }
 
